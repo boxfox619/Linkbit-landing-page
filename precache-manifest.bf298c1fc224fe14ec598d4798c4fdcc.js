b2579a4774ca9dbae8fe6b12be6f85082566c18b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-01 14:49:06 +0000

    Deploy boxfox619/Linkbit-landing-page to github.com/boxfox619/Linkbit-landing-page.git:gh-pages

---
+++ precache-manifest.bf298c1fc224fe14ec598d4798c4fdcc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0973df1d2fa6d851eac84b2519b71d6e",
+    "url": "/Linkbit-landing-page/index.html"
+  },
+  {
+    "revision": "683a14d988989c5df9c5",
+    "url": "/Linkbit-landing-page/static/css/main.3092de0a.chunk.css"
+  },
+  {
+    "revision": "8c62e54f302a01dbf9a5",
+    "url": "/Linkbit-landing-page/static/js/2.3db54211.chunk.js"
+  },
+  {
+    "revision": "683a14d988989c5df9c5",
+    "url": "/Linkbit-landing-page/static/js/main.c4a7a459.chunk.js"
+  },
+  {
+    "revision": "25e7d4282b380e2ca888",
+    "url": "/Linkbit-landing-page/static/js/runtime~main.0a3df88c.js"
+  },
+  {
+    "revision": "77a56bfc522175ddaf962c3b2447bf21",
+    "url": "/Linkbit-landing-page/static/media/ic_atom.77a56bfc.svg"
+  },
+  {
+    "revision": "d70c5315f98f38ce99e61e5b47783ba4",
+    "url": "/Linkbit-landing-page/static/media/ic_lightbulb.d70c5315.svg"
+  },
+  {
+    "revision": "759231fc1f899153cfeb5963380f57c7",
+    "url": "/Linkbit-landing-page/static/media/ic_linkbit.759231fc.svg"
+  },
+  {
+    "revision": "1318e2fbe4b890b93b5e27967eb2876e",
+    "url": "/Linkbit-landing-page/static/media/ic_linkbit_logo.1318e2fb.svg"
+  },
+  {
+    "revision": "668fa1b956a7df6e6651daf9606fc375",
+    "url": "/Linkbit-landing-page/static/media/ic_security.668fa1b9.svg"
+  },
+  {
+    "revision": "19ee4bef44050e54a84ef023d3121307",
+    "url": "/Linkbit-landing-page/static/media/img-linkbit-main.19ee4bef.png"
+  },
+  {
+    "revision": "ea6fdc2ccfbe0ed00ef6c1bf749c609c",
+    "url": "/Linkbit-landing-page/static/media/img_linkbit_story_1.ea6fdc2c.svg"
+  },
+  {
+    "revision": "ffd141f2a3123db78136ce7c8abe3028",
+    "url": "/Linkbit-landing-page/static/media/img_linkbit_story_2.ffd141f2.svg"
+  },
+  {
+    "revision": "f29245dcda0f7698fdb3d96ffe023359",
+    "url": "/Linkbit-landing-page/static/media/img_linkbit_story_3.f29245dc.svg"
+  }
+]);
